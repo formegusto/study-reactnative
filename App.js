99a4d4c55966ad9d31df99fad42c9f37dd3b6dfe
formegusto <formegusto@gmail.com>
2021-03-28 17:23:35 +0900

    Text Component

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useCallback } from "react";
-import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
-  const onPress = useCallback(() => {
-    Alert.alert("React Native Hi!");
+  const [content, setContent] = useState("");
+
+  const onChange = useCallback((text) => {
+    setContent(text);
   }, []);
+
+  const onPress = useCallback(() => {
+    Alert.alert(content);
+  }, [content]);
 
   return (
     <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="텍스트를 입력해주세요!"
+        value={content}
+        onChangeText={onChange}
+      />
       <TouchableOpacity activeOpacity={0.8} onPress={onPress}>
         <View style={styles.box}>
           <Text style={styles.first}>시작하기</Text>
@@ -24,6 +43,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    padding: 16,
   },
   box: {
     flexDirection: "row",
@@ -35,5 +55,15 @@ const styles = StyleSheet.create({
     color: "#FFF",
     fontWeight: "600",
     padding: 16,
+  },
+  input: {
+    width: "100%",
+    textAlign: "center",
+    margin: 16,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: "#CCC",
+    height: 32,
   },
 });
