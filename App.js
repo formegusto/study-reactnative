5f21ffe22a8587685c23da74440ff63a254c103b
formegusto <formegusto@gmail.com>
2021-03-28 16:54:27 +0900

    First React Native

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text>Hello! React Native!!</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    backgroundColor: "#0ff",
+  },
+});
