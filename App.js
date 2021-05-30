0f490a0a7332dc4c879957d84cfc992d80bd0c04
formegusto <formegusto@gmail.com>
2021-05-30 09:23:19 +0900

    Header

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import React from "react";
-import BasicContainer from "./containers/BasicContainer";
+import { SafeAreaView } from "react-native";
+import Header from "./components/Header";
 
 export default function App() {
-  return <BasicContainer />;
+  return (
+    <SafeAreaView>
+      <Header />
+    </SafeAreaView>
+  );
 }
+
+/*
+  SafeAreaView : 
+    상태바를 제외한 안전한 부분에 뷰를 배치 시켜주는 컴포넌트
+*/
